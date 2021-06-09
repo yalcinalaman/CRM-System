81863529404f4f4c8752dbd07ec875656bda7a7c
Muhammed Bedir ULUCAY <55952021+MuhammedBedirULUCAY@users.noreply.github.com>
2021-06-10 01:52:21 +0300

    Update Database.java

--- DataBaseCustomer/Database.java
+++ DataBaseCustomer/Database.java
@@ -1,93 +1,59 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Properties;
-
-import com.mysql.cj.xdevapi.Statement;
-// import com.mysql.jdbc.Connection;
+import java.sql.*;
 
 public class Database{
-
-
-    private static final String DATABASE_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/database_name";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "admin";
-    private static final String MAX_POOL = "250"; // set your own limit
-
-    // init connection object
-    private Connection connection = null;
-    // init properties object
-    private Properties properties = null;
 
     public Database(){
 
     }
 
-    public Connection connect() {
-        if (connection == null) {
-            try {
-                Class.forName(DATABASE_DRIVER);
-                connection = DriverManager.getConnection(DATABASE_URL, getProperties());
-            } catch (ClassNotFoundException | SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return connection;
-    }
-
-    private Properties getProperties() {
-
-        if (properties == null) {
-            properties = new Properties();
-            properties.setProperty("user", USERNAME);
-            properties.setProperty("password", PASSWORD);
-            properties.setProperty("MaxPooledStatements", MAX_POOL);
-        }
-        return properties;
-    }
-
-    public static boolean addCustomer(Customer c){
+    public static String addCustomer(Customer c){
         
         StringBuilder sb = new StringBuilder();
-        sb.append("INSERT INTO Customers VALUES ");
+        sb.append("INSERT INTO user VALUES ");
 
         sb.append("(");
-        sb.append(c.getID());
-        sb.append(c.getName());
-        sb.append(c.getSurName());
-        sb.append(c.getEmail());
-        sb.append(c.getPassword());
-        sb.append(c.getPhoneNumber());
+        sb.append("'" + c.getID() + "',");
+        sb.append("'" + c.getName() + ",");
+        sb.append("'" + c.getSurName() + ",");
+        sb.append("'" + c.getPassword() + ",");
+        sb.append("'" + c.getEmail() + ",");
+        sb.append("'" + c.getPhoneNumber());
         sb.append(");");
 
-        return false;
+        return sb.toString();
     }
-    
-    public static Customer removeCustomer(Customer c){
+
+    public static String removeCustomer(Customer c){
     
         StringBuilder sb = new StringBuilder();
         
-        sb.append("DELETE FROM Customers WHERE ");
-        sb.append(c.getEmail() + "=" + c.getEmail());
+        sb.append("DELETE FROM user WHERE ");
+        sb.append("email" + "= '" + c.getEmail()+"'");
     
-        return null;
+        return sb.toString();
     
     }
+
+
+    private static final String DATABASE_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DATABASE_URL = "jdbc:mysql://192.168.1.172:3306/customer_user";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
 
     public static void main(String[] args){
 
         try{
+
+            Customer c = new Customer();
+            Class.forName(DATABASE_DRIVER);
             
-            Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
             Statement statement = (Statement) connection.createStatement();
-            ResultSet resultSet = (ResultSet) statement.execute();
 
-            while(resultSet.next()){
-                System.out.println(resultSet.getString(0));
-            }
+            String add = addCustomer(c);
+            int resultSet =  statement.executeUpdate(add);
+            System.out.println(resultSet);
 
         }catch(Exception e){
             e.printStackTrace();
